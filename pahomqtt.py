03d2dfc2ede6868b9a3d92c0e47b4a249dedbb64
codexpress18 <codexpress18@gmail.com>
2018-07-23 20:05:00 +0800

    Updated MQTT code

--- pahomqtt.py
+++ pahomqtt.py
@@ -1,18 +1,35 @@
 import paho.mqtt.client as mqtt
+import requests
 import time
+import json
+import logging
+
+logging.basicConfig(filename="logs.log" , level=logging.DEBUG)
+
+def timeNow():
+    return str(time.asctime(time.localtime())) 
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata,flags, rc):
     client.subscribe("esp/test")
-    print "The connection to the MQTT server is established."
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print msg.topic + " " + str(msg.payload)
+    logging.info(msg.topic + " " + str(msg.payload.decode()) + " " + str(time.asctime(time.localtime())))
     if msg.payload == 'exit':
-        print "Recieved exit command"
+        logging.info("Recieved exit command")
         client.disconnect()
-
+    else:
+        data = msg.payload.decode()
+        print(data, timeNow())
+        try:
+            r = requests.post("http://localhost:3000/data", json=json.loads(data))
+            if r and r.status_code == 200:
+                logging.info("Delivery at: " + str(time.asctime(time.localtime())))
+        
+        except json.JSONDecodeError:
+            logging.warning("Invalid JSON at: " + str(time.asctime(time.localtime())))
+        
 client = mqtt.Client("rbp")
 client.username_pw_set("pi","codexpress")
 client.on_connect = on_connect
